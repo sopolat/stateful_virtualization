861f5a96a33601f2f9aba0cb9d995d028e970794
HasanFerit <hasanferit@hasanferit-VB.(none)>
2017-06-18 00:23:33 +0300

    One of c encoding function added

--- response_generation_ml.py
+++ response_generation_ml.py
@@ -1,14 +1,54 @@
-from sklearn.multioutput import MultiOutputClassifier
-from sklearn.ensemble import RandomForestClassifier
-import json
 
 '''
 Response generation with machine learning. Data preperation.
 '''
 
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+import json
+
+def one_of_c_encoder(
+    request_types, request_data, response_data, unique_req_types):
+    '''
+    1-of-c encoding function 
+    '''
+    encoded_data = []
+    encoded_ouput = []
+    for j in range(len(request_types)):
+        req_type = request_types[j]
+        req_data = request_data[j]
+        res_data = response_data[j]
+
+        part_datapoint = [0 for k in range(len(unique_req_types))]
+        part_datapoint[unique_req_types.index(req_type)] = 1
+
+        encoded_data += part_datapoint
+
+        unique_corresponding_req_data = get_unique_data(
+            req_type, request_types_list, request_data_list)
+        for data in req_data:
+            part_datapoint = [0 for k in range(
+                len(unique_corresponding_req_data))]
+            part_datapoint[unique_corresponding_req_data.index(data)] = 1
+            encoded_data += part_datapoint
+
+        unique_corresponding_res_data = get_unique_data(
+            req_type, request_types_list, response_data_list)
+        for data in res_data:
+            part_datapoint = [0 for k in range(
+                len(unique_corresponding_res_data))]
+            part_datapoint[unique_corresponding_res_data.index(data)] = 1
+
+            if j == len(request_types) - 1:
+                encoded_ouput = part_datapoint
+            else:
+                encoded_data += part_datapoint
+
+    # print len(encoded_data)
+    return encoded_data, encoded_ouput
+
 
 def get_unique_data(req_type, request_types_list, data_list):
-
     '''
     Get unique request or response data of a given request type.
     '''
@@ -31,55 +71,27 @@ def get_unique_data(req_type, request_types_list, data_list):
     return unique_corresponding_data
 
 
-
 traces = open('ml_traces', 'rb')
 data = json.load(traces)
 request_types_list = data['request_types'][:-1]
-request_data_list  = data['request_data'][:-1]
+request_data_list = data['request_data'][:-1]
 response_data_list = data['response_data'][:-1]
 
 unique_req_types = list(set(
-    [req_type for request_types in request_types_list for req_type in request_types]))
+    [req_type for request_types in request_types_list 
+    for req_type in request_types]))
 
 datapoints = []
 outputs = []
 
 for i in range(len(request_types_list)):
-    datapoint = []
-    output = []
+
     request_types = request_types_list[i]
-    request_data  = request_data_list[i]
+    request_data = request_data_list[i]
     response_data = response_data_list[i]
-    for j in range(len(request_types)):
-        req_type = request_types[j]
-        req_data = request_data[j]
-        res_data = response_data[j]
 
-        part_datapoint = [0 for k in range(len(unique_req_types))]
-        part_datapoint[unique_req_types.index(req_type)] = 1
-
-        datapoint += part_datapoint
-
-        unique_corresponding_req_data = get_unique_data(
-            req_type, request_types_list, request_data_list)
-        for data in req_data:
-            part_datapoint = [0 for k in range(
-                len(unique_corresponding_req_data))]
-            part_datapoint[unique_corresponding_req_data.index(data)] = 1
-            datapoint += part_datapoint
-
-        unique_corresponding_res_data = get_unique_data(
-            req_type, request_types_list, response_data_list)
-        for data in res_data:
-            part_datapoint = [0 for k in range(
-                len(unique_corresponding_res_data))]
-            part_datapoint[unique_corresponding_res_data.index(data)] = 1
-
-            if j == len(request_types) - 1:
-                output = part_datapoint
-            else:
-                datapoint += part_datapoint
-
+    datapoint, output = one_of_c_encoder(
+        request_types, request_data, response_data, unique_req_types)
 
     datapoints.append(datapoint)
     outputs.append(output)
@@ -88,55 +100,54 @@ forest = RandomForestClassifier(n_estimators=10)
 multi_target_forest = MultiOutputClassifier(forest)
 classifier = multi_target_forest.fit(datapoints, outputs)
 
-traces = open('ml_traces', 'rb')
-data = json.load(traces)
-request_types_list = [data['request_types'][-1]]
-request_data_list  = [data['request_data'][-1]]
-response_data_list = [data['response_data'][-1]]
+# traces = open('ml_traces', 'rb')
+# data = json.load(traces)
+# request_types_list = [data['request_types'][-1]]
+# request_data_list  = [data['request_data'][-1]]
+# response_data_list = [data['response_data'][-1]]
 
-datapoints = []
-outputs = []
+# datapoints = []
+# outputs = []
 
-for i in range(len(request_types_list)):
-    datapoint = []
-    output = []
-    request_types = request_types_list[i]
-    request_data  = request_data_list[i]
-    response_data = response_data_list[i]
-    for j in range(len(request_types)):
-        req_type = request_types[j]
-        req_data = request_data[j]
-        res_data = response_data[j]
+# for i in range(len(request_types_list)):
+#     datapoint = []
+#     output = []
+#     request_types = request_types_list[i]
+#     request_data  = request_data_list[i]
+#     response_data = response_data_list[i]
+#     for j in range(len(request_types)):
+#         req_type = request_types[j]
+#         req_data = request_data[j]
+#         res_data = response_data[j]
 
-        part_datapoint = [0 for k in range(len(unique_req_types))]
-        part_datapoint[unique_req_types.index(req_type)] = 1
+#         part_datapoint = [0 for k in range(len(unique_req_types))]
+#         part_datapoint[unique_req_types.index(req_type)] = 1
 
-        datapoint += part_datapoint
+#         datapoint += part_datapoint
 
-        unique_corresponding_req_data = get_unique_data(
-            req_type, request_types_list, request_data_list)
-        for data in req_data:
-            part_datapoint = [0 for k in range(
-                len(unique_corresponding_req_data))]
-            part_datapoint[unique_corresponding_req_data.index(data)] = 1
-            datapoint += part_datapoint
+#         unique_corresponding_req_data = get_unique_data(
+#             req_type, request_types_list, request_data_list)
+#         for data in req_data:
+#             part_datapoint = [0 for k in range(
+#                 len(unique_corresponding_req_data))]
+#             part_datapoint[unique_corresponding_req_data.index(data)] = 1
+#             datapoint += part_datapoint
 
-        unique_corresponding_res_data = get_unique_data(
-            req_type, request_types_list, response_data_list)
-        for data in res_data:
-            part_datapoint = [0 for k in range(
-                len(unique_corresponding_res_data))]
-            part_datapoint[unique_corresponding_res_data.index(data)] = 1
+#         unique_corresponding_res_data = get_unique_data(
+#             req_type, request_types_list, response_data_list)
+#         for data in res_data:
+#             part_datapoint = [0 for k in range(
+#                 len(unique_corresponding_res_data))]
+#             part_datapoint[unique_corresponding_res_data.index(data)] = 1
 
-            if j == len(request_types) - 1:
-                output = part_datapoint
-            else:
-                datapoint += part_datapoint
+#             if j == len(request_types) - 1:
+#                 output = part_datapoint
+#             else:
+#                 datapoint += part_datapoint
 
 
-    datapoints.append(datapoint)
-    outputs.append(output)
+#     datapoints.append(datapoint)
+#     outputs.append(output)
 
-print '------------------'
-classifier.predict(datapoints)
-
+# print '------------------'
+# classifier.predict(datapoints)
