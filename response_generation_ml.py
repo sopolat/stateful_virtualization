253727608f1268dc21f47703c80cbba48679b747
HasanFerit <hasanferit@hasanferit-VB.(none)>
2017-08-13 23:33:57 +0300

    mine pump controller data set configurations

--- response_generation_ml.py
+++ response_generation_ml.py
@@ -3,7 +3,7 @@
 Response generation with machine learning. Data preperation.
 '''
 import numpy as np
-# from sklearn.neural_network import MLPClassifier, MLPRegressor
+from sklearn.neural_network import MLPClassifier, MLPRegressor
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.gaussian_process import GaussianProcessClassifier
@@ -18,260 +18,154 @@ import json
 import sys
 import os
 
-
-def pre_process_data(request_types_list, request_data_list, response_data_list):
-    '''
-    Mines needed information from the data
-    '''
-    unique_req_types = list(set(
-        [req_type for request_types in request_types_list
-         for req_type in request_types]))
-
-    req_type_datapoint_dict = {}
-    for req_type in unique_req_types:
-        # part_datapoint = [0] * len(unique_req_types)
-        # part_datapoint[unique_req_types.index(req_type)] = -1
-        part_datapoint = [unique_req_types.index(req_type) + 1]
-        req_type_datapoint_dict[req_type] = part_datapoint
-
-    req_data_datapoint_dict = {}
-    max_req_data_len = 0
-    for req_type in unique_req_types:
-        unique_corresponding_req_data = get_unique_data(
-            req_type, request_types_list, request_data_list)
-        if len(unique_corresponding_req_data) > max_req_data_len:
-            max_req_data_len = len(unique_corresponding_req_data)
-        for data in unique_corresponding_req_data:
-            part_datapoint = [0] * len(unique_corresponding_req_data)
-            part_datapoint[unique_corresponding_req_data.index(data)] = -1
-            req_data_datapoint_dict[(req_type, str(data))] = part_datapoint
-
-    for value in req_data_datapoint_dict.values():
-        value += (max_req_data_len - len(value)) * [0]
-
-    res_data_datapoint_dict = {}
-    max_res_data_len = 0
-    for req_type in unique_req_types:
-        unique_corresponding_res_data = get_unique_data(
-            req_type, request_types_list, response_data_list)
-        unique_corresponding_res_data += [str([])]
-        if len(unique_corresponding_res_data) > max_res_data_len:
-            max_res_data_len = len(unique_corresponding_res_data)
-        for data in unique_corresponding_res_data:
-            part_datapoint = [0] * len(unique_corresponding_res_data)
-            part_datapoint[unique_corresponding_res_data.index(data)] = -1
-            res_data_datapoint_dict[(req_type, str(data))] = part_datapoint
-
-    for value in res_data_datapoint_dict.values():
-
-        value += (max_res_data_len - len(value)) * [0]
-
-    print req_type_datapoint_dict
-    print ''
-    print req_data_datapoint_dict
-    print ''
-    print res_data_datapoint_dict
-    return req_type_datapoint_dict, req_data_datapoint_dict, res_data_datapoint_dict, max_req_data_len, max_res_data_len
+######################
+#Sample command
+#python critical 5 100
+######################
 
 
-fw = open('debug_datapoint', 'w')
+#global variables
+operation_to_be_trained = sys.argv[1]
+trace_size = int(sys.argv[2])
 
 
-def one_hot_encoder(req_type_datapoint_dict,
-        req_data_datapoint_dict, res_data_datapoint_dict, max_req_data_len,
-        max_res_data_len, request_types, request_data, response_data):
-    '''
-    one-hot encoding function 
-    '''
-    encoded_data = []
-    # encoded_ouput = []
-    for j in range(len(request_types)):
-        req_type = request_types[j]
-        req_data = request_data[j]
-        res_data = response_data[j]
-
-        part_datapoint = req_type_datapoint_dict[req_type]
-        encoded_data += part_datapoint
-
-        # print 'REQ TYPE ' + str(j) + ':'
-        # print part_datapoint
-        fw.write(str(part_datapoint))
-        fw.write(str(len(encoded_data)))
-        fw.write('-')
-        # for data in req_data:
-        part_datapoint = req_data_datapoint_dict[(req_type, str(req_data))]
-        encoded_data += part_datapoint
-        # print 'REQ DATA ' + str(j)
-        # print part_datapoint
-
-        fw.write(str(part_datapoint))
-        fw.write(str(len(encoded_data)))
-        fw.write('-')
-
-        # print res_data
-        # for data in res_data:
-
-        part_datapoint = res_data_datapoint_dict[(req_type, str(res_data))]
-        if j == len(request_types) - 1:
-            last_set = len(request_types) - 1 - request_types[::-1].index('service/set/')
-            encoded_ouput =  req_data_datapoint_dict[('service/set/', str(request_data[last_set]))] #last_set #request_types.count('service/add/')  #len(res_data) #part_datapoint[0]
-        else:
-            encoded_data += part_datapoint
-
-        fw.write(str(part_datapoint))
-        fw.write(str(len(encoded_data)))
-        fw.write(' ')
-
-    return encoded_data, encoded_ouput
 
 
-def output_encoder(unique_corresponding_req_data, request_data, data):
-    flat_request_data = [item for sublist in request_data for item in sublist]
-    # encoded_output = []
-    # test for only the first name. ignore phone number and surname
-    # data = res_data[0]
-    # for data in res_data[0]:
-    # print data
-    # print flat_request_data
-    # print unique_corresponding_req_data
-    if data in flat_request_data:
-        # part_datapoint = [0] * len(unique_corresponding_req_data)
-        # if data in unique_corresponding_req_data:
-        #     part_datapoint[unique_corresponding_req_data.index(data)] = 1
-        encoded_output = unique_corresponding_req_data.index(data)
-        # encoded_output = [0] * len(flat_request_data)
-        # encoded_output[flat_request_data.index(data)] = 1
+def arrange_trace_window(event_list, param_list, event, water_level, methane_level, is_pump):
+    
+    event_list.append(event)
+    param_list.append([water_level, methane_level, is_pump])
+    if len(event_list) > trace_size:
+        del event_list[0]
 
-    return encoded_output
+    if len(param_list) > trace_size:
+        del param_list[0]
 
-
-def get_unique_data(req_type, request_types_list, data_list):
-    '''
-    Get unique request or response data of a given request type.
-    '''
-    corresponding_data = []
-    for i in range(len(request_types_list)):
-        request_types = request_types_list[i]
-        data = data_list[i]
-
-        indices = [k for k, item in enumerate(
-            request_types) if item == req_type]
-
-        part_corresponding_data = [
-            data[index] for index in indices]
-        # part_corresponding_data_flat = [
-        #     item for sublist in part_corresponding_data for item in sublist]
-        corresponding_data += part_corresponding_data
-
-    if isinstance(corresponding_data[0], list):
-        # unique_corresponding_data = [list(x) for x in set(
-        #     tuple(x) for x in corresponding_data)]
-        # print corresponding_data
-        unique_corresponding_data = []
-        for x in corresponding_data:
-            if x not in unique_corresponding_data:
-                unique_corresponding_data.append(x)
-        print unique_corresponding_data
-    else:
-        unique_corresponding_data = list(set(corresponding_data))
-    # print unique_corresponding_data
-    return unique_corresponding_data
+    return event_list, param_list
 
 #######################################
 ############TRAINING PART##############
 #######################################
 
-# Crete data first
-cmd = 'python2 data_creator_add_delete.py ' + sys.argv[1] + ' ' + sys.argv[2]
-os.system(cmd)
+#Data is ready. Taken from Walkinshaw
+mine_pump_data = open('mine_pump', 'rb')
 
-traces = open('ml_service_traces', 'rb')
-data = json.load(traces)
 
-request_types_list = data['request_types']
-request_data_list = data['request_data']
-response_data_list = data['response_data']
+fw = open('debug_datapoint', 'w')
 
-# unique_req_types = list(set(
-#     [req_type for request_types in request_types_list
-#      for req_type in request_types]))
+event_types = ['highwater',
+'switch_pump_on',
+'turn_on',
+'critical',
+'switch_pump_off',
+'turn_off',
+'not_critical',
+'low_water']
 
+event_list = []
+param_list = []
 datapoints = []
 outputs = []
-total_length = len(request_types_list)
-max_len = 0
+next_event_flag = False
+while True:
+    line = mine_pump_data.readline()
+    if not line: break  # EOF
+    if 'trace' in line:
+        #remove last datapoint. becaues there is no next event
+        if next_event_flag:
+            del datapoints[-1]
+            next_event_flag = False
+        event_list = []
+        param_list = []
+        continue
+    line = line.strip()
+    line_splitted = line.split(' ')
+    event = line_splitted[0]
+    water_level = float(line_splitted[1])
+    methane_level = float(line_splitted[2])
+    
+    if line_splitted[3] == 'true':
+        is_pump = 1 
+    else:
+        is_pump = 0
 
-req_type_datapoint_dict, req_data_datapoint_dict, \
-res_data_datapoint_dict, max_req_data_len, max_res_data_len = \
-pre_process_data(request_types_list, request_data_list, response_data_list)
+    if next_event_flag:
+        # output_encoded = [0] * len(event_types)
+        # output_encoded[event_types.index(event)] = -1
+        # outputs.append(output_encoded)
+        outputs.append(event_types.index(event))
+        fw.write(str(outputs))
+        fw.write('\n')
+        next_event_flag = False
 
-for i in range(total_length):
-    request_types = request_types_list[i]
-    request_data = request_data_list[i]
-    response_data = response_data_list[i]
+    event_list, param_list = arrange_trace_window(event_list, param_list, event, water_level, methane_level, is_pump)
 
-    datapoint, output = one_hot_encoder(req_type_datapoint_dict,
-                                        req_data_datapoint_dict, res_data_datapoint_dict, max_req_data_len,
-                                        max_res_data_len, request_types, request_data, response_data)
+    if event == operation_to_be_trained and len(event_list)== trace_size:
+        datapoint = []
+        next_event_flag = True
+        for event, params in zip(event_list, param_list):
+            event_encoded = [0] * len(event_types)
+            event_encoded[event_types.index(event)] = -1
+            datapoint += event_encoded
+            for param in params:
+                datapoint += [param]
+        fw.write(str(datapoint))
+        datapoints.append(datapoint)
 
-    fw.write('\n')
-    # print len(datapoint)
-    # print len(output)
-    datapoints.append(datapoint)
-    outputs.append(output)
-    # print i
+# exit()      
 
-    if len(datapoint) > max_len:
-        max_len = len(datapoint)
+
 fw.close()
 
+# Dimensionality reduction
 # datapoints = list(PCA(n_components=100).fit_transform(datapoints))
 # print 'data reduced (PCA)'
 
 names = [
-        #"Decision Tree", 
+        "Decision Tree", 
          "Linear SVM",  # 
          # "Neural Net", #"Nearest Neighbors", 
          # "RBF SVM", 
          # "Random Forest", #"Naive Bayes", "QDA"
-         # "AdaBoost",
+         "AdaBoost",
          # "Gaussian Process"
          ]
 classifiers = [
     # KNeighborsClassifier(3),
-    # DecisionTreeClassifier(max_depth=5),
-    SVC(kernel="linear", C=0.025),
+    DecisionTreeClassifier(max_depth=18),
+    SVC(kernel="linear", C=0.005),
     # SVC(gamma=2, C=1),
     # MLPClassifier(alpha=1),
     # GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True),
     # RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-    # AdaBoostClassifier(),
+    AdaBoostClassifier(),
     # GaussianNB(),
     # QuadraticDiscriminantAnalysis()
     ]
 
+total_length = len(datapoints)
+kfold = int(sys.argv[3])
+CROSS_VAL_SIZE = int(total_length / kfold)
+
+print len(datapoints)
+print len(outputs)
+print CROSS_VAL_SIZE
 
 print 'train is over. test starts here.'
 for name, clf in zip(names, classifiers):
-    # clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(100, ),
-    #                     random_state=1, activation='logistic', max_iter=1000)
-
-    # clf = AdaBoostClassifier()
-    # multi_target_forest = MultiOutputRegressor(mlp)
-    # classifier = multi_target_forest.fit(datapoints, outputs)
 
     print name
     # Read cross val size from the user input
-    CROSS_VAL_SIZE = int(sys.argv[3])
     outfile = file('outfile_ml' + name, 'w')
     outfile.write('Out Predicted')
     outfile.write('\n')
     wrong_guess_counter = 0
     # try:
-    for k in range(total_length / CROSS_VAL_SIZE):
+    for k in range(kfold):
         clf.fit(datapoints[(k + 1) * CROSS_VAL_SIZE:] + datapoints[:k * CROSS_VAL_SIZE],
                 outputs[(k + 1) * CROSS_VAL_SIZE:] + outputs[:k * CROSS_VAL_SIZE])
         print 'fitted'
+
         #######################################
         ##############TEST PART################
         #######################################
@@ -279,15 +173,12 @@ for name, clf in zip(names, classifiers):
         for i in range((k * CROSS_VAL_SIZE), ((k + 1) * CROSS_VAL_SIZE)):
             datap = np.array(datapoints[i]).reshape(1, -1)
             predicted = clf.predict(datap)
-            # print outputs[i]
-            # print predicted
+
             if outputs[i] != predicted.tolist()[0]:
                 wrong_guess_counter += 1
 
             outfile.write(str(outputs[i]) + ' ' + str(predicted))
             outfile.write('\n')
-            # print str(outputs[i])  + ' ' + str(predicted)# true response vs
-            # predicted
 
     outfile.write('\n')
     outfile.write(str(wrong_guess_counter))
